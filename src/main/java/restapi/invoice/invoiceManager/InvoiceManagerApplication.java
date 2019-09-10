f39eab35c0cb438d769bcb27be5b56b510877a7f
sayantjm <sayantjm@gmail.com>
2019-09-10 22:23:26 +0200

    Initial commit

---
+++ src/main/java/restapi/invoice/invoiceManager/InvoiceManagerApplication.java
@@ -0,0 +1,13 @@
+package restapi.invoice.invoiceManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restapi/invoice/invoiceManager/InvoiceManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package restapi.invoice.invoiceManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
