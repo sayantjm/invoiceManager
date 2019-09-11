938f5fc297a09825f2a39905655ee124f6104e23
sayantjm <sayantjm@gmail.com>
2019-09-11 20:47:19 +0200

    Added entity Construction, the dto and its tests

---
+++ src/main/java/restapi/invoice/invoiceManager/dto/ConstructionDTO.java
@@ -0,0 +1,11 @@
+package restapi.invoice.invoiceManager.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ConstructionDTO {
+    private Long id;
+    private String name;
+}
