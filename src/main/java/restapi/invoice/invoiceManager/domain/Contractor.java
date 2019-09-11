c2ef7d8b469beb293c6441012ccd867036d9bd97
sayantjm <sayantjm@gmail.com>
2019-09-11 21:14:18 +0200

    Added entity Contractor, the dto and its tests

--- src/main/java/restapi/invoice/invoiceManager/domain/Contractor.java
+++ src/main/java/restapi/invoice/invoiceManager/domain/Contractor.java
@@ -1,7 +1,10 @@
 package restapi.invoice.invoiceManager.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import restapi.invoice.invoiceManager.dto.ContractorDTO;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,16 +13,42 @@ import javax.persistence.Table;
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="CONTRACTOR")
 public class Contractor {
     @Id
-    Long id;
+    private Long id;
 
-    String name;
-    String dni;
-    String address;
-    String city;
-    Integer codPostal;
-    String state;
-    String bankAccount;
+    private String name;
+    private String dni;
+    private String address;
+    private String city;
+    private Integer codPostal;
+    private String state;
+    private String bankAccount;
+
+    public ContractorDTO toDto() {
+        return ContractorDTO.builder()
+                .id(this.id)
+                .name(this.name)
+                .dni(this.dni)
+                .address(this.address)
+                .city(this.city)
+                .codPostal(this.codPostal)
+                .state(this.state)
+                .bankAccount(this.bankAccount)
+                .build();
+    }
+
+    public void fromDto(ContractorDTO contractorDTO) {
+        this.id = contractorDTO.getId();
+        this.name = contractorDTO.getName();
+        this.dni = contractorDTO.getDni();
+        this.address = contractorDTO.getAddress();
+        this.city = contractorDTO.getCity();
+        this.codPostal = contractorDTO.getCodPostal();
+        this.state = contractorDTO.getState();
+        this.bankAccount = contractorDTO.getBankAccount();
+    }
 }
